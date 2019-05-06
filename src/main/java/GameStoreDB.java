0c3fb08edb7c03a7784442cbb76919f501b32b0e
Nick <cloud@gmail.com>
2019-05-06 14:10:14 -0500

    appearance changes, comboBoxes loaded, Jtable configured for products.

--- src/main/java/GameStoreDB.java
+++ src/main/java/GameStoreDB.java
@@ -1,17 +1,86 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
 
+// database communications
 public class GameStoreDB {
 
-    public static void connection() {
+    private static final String GET_CONSOLES = "SELECT * FROM inventory WHERE category LIKE 'Consoles'";
+
+    public static ArrayList<String> getCategories() {
+
+        ArrayList<String> allCategories = new ArrayList<>();
+        ArrayList<String> duplicateCategoriesRemoved = new ArrayList<>();
 
         try (Connection connection = DriverManager.getConnection(GameStoreConfigDB.gameStoreDb_url)) { // Connect
             Statement statement = connection.createStatement(); // Statements are used to issue queries
 
+            String getCategories = "SELECT CATEGORY FROM inventory"; // Query to fetch data
+            ResultSet categories = statement.executeQuery(getCategories); // Use executeQuery. It returns a ResultSet
+
+            while (categories.next()) {  // Have to loop over the ResultSet to read it. Loop reads one row at a time
+                String productName = categories.getString("CATEGORY"); // Can get data from each column, by column name
+                allCategories.add(productName);
+            }
+            categories.close(); // Close ResultSet when done using it
+
         } catch (SQLException sqle) {
             System.out.println(sqle);
         }
+        // for loop to remove duplicate strings from allCategories list
+        for (String cat : allCategories) {
+            // If this element is not present in newList then add it
+            if (!duplicateCategoriesRemoved.contains(cat)) {
+                duplicateCategoriesRemoved.add(cat);
+            }
+        }
+        return duplicateCategoriesRemoved;
+
+    }
+
+    public static Vector getColumnNames() {
+
+        Vector<String> colNames = new Vector<String>();
+
+        colNames.add("Product Image");
+        colNames.add("Product");
+        colNames.add("Price");
+
+        return colNames;
+    }
+
+
+    public static Vector<Vector> getConsoles() {
+
+        try (Connection connection = DriverManager.getConnection(GameStoreConfigDB.gameStoreDb_url);
+             Statement statement = connection.createStatement()) {
+
+            ResultSet rs = statement.executeQuery(GET_CONSOLES);
+
+            Vector<Vector> vectors = new Vector<>();
+
+            String image;
+            String name;
+            double price;
+
+            while (rs.next()) {
+
+                image = rs.getString("PRODUCT_IMAGE");
+                name = rs.getString("PRODUCT_NAME");
+                price = rs.getDouble("PRICE");
+
+                Vector v = new Vector();
+                v.add(image);
+                v.add(name);
+                v.add(price);
+
+                vectors.add(v);
+            }
+            return vectors;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/java/GameStoreGUI.java
+++ src/main/java/GameStoreGUI.java
@@ -1,5 +1,83 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class GameStoreGUI extends JFrame {
     private JPanel mainPanel;
+    private JPanel ProductBrowserPanel, CheckOutPanel, ShoppingCartPanel;
+    private JTextField customerNameTextField, shippingAddressTextField;
+    private JComboBox<String> CategoriesOptionsBox;
+    private JComboBox<String> SortingOptionsBox;
+    private JButton placeOrderButton;
+    private JTable ProductBrowserTable;
+
+    GameStoreGUI() {
+        setContentPane(mainPanel); // opens entire GUI form when run
+        pack();
+        setTitle("Couch Potato Shopping Application");
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // opens window to fullscreen
+//        setUndecorated(false); // keeps top menu bar for closing/min/maximizing etc.
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // ends program when window is closed
+        setVisible(true); // makes window visible when program is run
+
+        eventHandlers(); // configure event handlers for GUI interaction
+
+        // calls method that will get list of category options from inventory table
+        ArrayList<String> inventoryCategories = GameStoreDB.getCategories();
+
+        // Categories assigned to individual variables from array
+        String all = "All";
+        String consoles = inventoryCategories.get(0);
+        String accessories = inventoryCategories.get(1);
+        String games = inventoryCategories.get(2);
+
+        // adds category options to comboBox
+        CategoriesOptionsBox.addItem(all);
+        CategoriesOptionsBox.addItem(consoles);
+        CategoriesOptionsBox.addItem(accessories);
+        CategoriesOptionsBox.addItem(games);
+
+        // Variables to hold sorting options
+        String lowPrice = "Price: Low to high";
+        String highPrice = "Price: High to low";
+        String alphabetical = "Alphabetical";
+
+        // adds sorting options to comboBox
+        SortingOptionsBox.addItem(lowPrice);
+        SortingOptionsBox.addItem(highPrice);
+        SortingOptionsBox.addItem(alphabetical);
+
+    }
+
+    private void configureProductTable() {
+
+        Vector columnNames = GameStoreDB.getColumnNames();
+        Vector<Vector> data = GameStoreDB.getConsoles();
+
+        DefaultTableModel tableModel = new DefaultTableModel(data, columnNames);
+        ProductBrowserTable.setModel(tableModel);
+
+    }
+
+
+    private void eventHandlers() {
+
+        CategoriesOptionsBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String selectedCategory = (String) CategoriesOptionsBox.getSelectedItem();
+
+                if (selectedCategory.equals("Consoles")) {
+                    configureProductTable();
+                }
+
+            }
+        });
+
+    }
+
 }
